5b0aeab6b4a33ddc84332154522a5d4a3acd8d93
venky <snikommuvenky115@gmail.com>
2024-09-10 22:42:08 +0100

    ssh hardening

--- scripts/ssh_hardening.sh
+++ scripts/ssh_hardening.sh
@@ -8,4 +8,4 @@ sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sudo sed -i 's/#Port 22/Port 2222/' /etc/ssh/sshd_config
 
 # Restart SSH service
-sudo systemctl restart sshd
+sudo systemctl restart ssh
